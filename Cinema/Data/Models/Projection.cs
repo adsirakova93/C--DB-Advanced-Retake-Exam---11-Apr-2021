115b3b11c03d2f1b35c9ce5cd9c7bcdea1f316c8
ADSirakova93 <adsirakova.93@gmail.com>
2021-04-11 12:18:29 +0300

    Initial commit

---
+++ Cinema/Data/Models/Projection.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cinema.Data.Models
+{
+    public class Projection
+    {
+        public Projection()
+        {
+            this.Tickets = new HashSet<Ticket>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        public int MovieId { get; set; }
+
+        public Movie Movie { get; set; }
+
+
+        public DateTime DateTime { get; set; }
+
+        public virtual ICollection<Ticket> Tickets { get; set; }
+    }
+}
